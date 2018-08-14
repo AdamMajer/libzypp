openSUSE/zypper

ProvideFilePolicy: Add a FileCecker to be passed down to the Fetcher

zypp/repo/RepoProvideFile.cc
@@ -307,7 +307,7 @@ namespace zypp
           MIL << "Providing file of repo '" << repo_r.alias() << "' from " << url << endl;
           shared_ptr<MediaSetAccess> access = _impl->mediaAccessForUrl( url, repo_r );
 
-	  fetcher.enqueue( locWithPath );
+	  fetcher.enqueue( locWithPath, policy_r.fileChecker() );
 	  fetcher.start( destinationDir, *access );
 
 	  // reached if no exception has been thrown, so this is the correct file

AdamMajer/libzypp

zypp/repo/RepoProvideFile.cc
@@ -305,7 +305,7 @@ namespace zypp
           MIL << "Providing file of repo '" << repo_r.alias() << "' from " << url << endl;
           shared_ptr<MediaSetAccess> access = _impl->mediaAccessForUrl( url, repo_r );
 
-	  fetcher.enqueue( loc_r );
+	  fetcher.enqueue( loc_r, policy_r.fileChecker() );
 	  fetcher.start( destinationDir, *access );
 
 	  // reached if no exception has been thrown, so this is the correct file